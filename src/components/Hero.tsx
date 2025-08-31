65e808c92a5a5bb41ed7550e3a804261b25c2842
Hyan K De Lima <hyankdelima@gmail.com>
2025-08-31 12:34:45 -0300

    Substituído ícone Bitcoin de color bg-primary pra bg-white SIZE - Hero.tsx - itera02.06

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,7 +22,7 @@ export const Hero = () => {
         <div className="animate-slide-up">
           <div className="flex justify-center mb-6">
             <div className="p-4 rounded-full bg-white animate-bitcoin-pulse">
-              <img src={BitcoinLogo} alt="Bitcoin Logo" className="w-10 h-10" />
+              <img src={BitcoinLogo} alt="Bitcoin Logo" className="w-13 h-12" />
             </div>
           </div>
           
